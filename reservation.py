3fe3f9c9cfc92e821e212ffa20d89db8348753bb
Alex T <sturmmanngr@gmail.com>
2022-05-31 11:30:09 +0700

    buttons now in line

--- reservation.py
+++ reservation.py
@@ -3,9 +3,9 @@ from discord.ext import commands
 from discord_components import DiscordComponents, ComponentsBot, Button, SelectOption, Select
 
 start_components = [
-    Button(label='USA', style=3, custom_id='usa_button'),
+    [Button(label='USA', style=3, custom_id='usa_button'),
     Button(label='Germany', style=3, custom_id='ger_button'),
-    Button(label='UK', style=3, custom_id='uk_button')
+    Button(label='UK', style=3, custom_id='uk_button')]
 ]
 
 
